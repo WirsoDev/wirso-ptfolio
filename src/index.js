2bee225b064658366633d44391f0dae3c3f1dcb1
wirsodev <wilson.wmarques@gmail.com>
2022-03-14 17:48:52 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Express = require('express')
+const nunjucks = require('nunjucks')
+
+
+const app = Express()
+
+app.use(Express.static('public'))
+
+nunjucks.configure('src/views', {
+    express: app,
+    noCache: true
+})
+
+
+app.use('/', require('./routes/main'))
+
+
+// start server
+var port = 3000
+app.listen(process.env.PORT || port, () => { console.log('Server up at port - ' + port) })
---
+++ src/routes/main.js
@@ -0,0 +1,9 @@
+const Express = require('express')
+const router = Express.Router()
+
+router.get('/', (req, res)=>{
+    return(res.render('main.html'))
+})
+
+
+module.exports = router
